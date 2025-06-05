ad1423d693484f8526e43325b1425c03da5d24cd
FredieTech <frediezra0@gmail.com>
2025-06-06 02:47:13 +0300

    Update logo1.js

--- plugins/logo1.js
+++ plugins/logo1.js
@@ -1,4 +1,4 @@
-const { fredi } = require("../fredi/ezra");
+const { ezra } = require("../fredi/ezra");
 const axios = require("axios");
 
 ezra({ nomCom: "logo", categorie: "Fredi-Search", reaction: "🤭" }, async (dest, zk, commandeOptions) => {
